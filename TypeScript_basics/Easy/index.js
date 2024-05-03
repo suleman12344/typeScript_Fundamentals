5d24d524992f03b0d5eefc919d556c1539750dcd
Muhammad Suleman <muhammadsulemanlahore@gmail.com>
2024-05-03 16:36:39 +0500

    init

---
+++ TypeScript_basics/Easy/index.js
@@ -0,0 +1,28 @@
+"use strict";
+//Write a script to log "Hello, World!" to the console.
+console.log("Hello, World!\n");
+//Create a variable temperature and assign it a value. Then log "It's cold!" if the temperature is below 20 degrees Celsius.
+let temperature = 50;
+if (temperature < 20) {
+    console.log("It' cold!\n");
+}
+else {
+    console.log("It's hot!\n");
+}
+//Use arithmetic operators to solve the problem: If you have 10 apples and you give away 3, how many do you have left? Log the result.
+let apples = 10;
+apples -= 3;
+console.log(`After giving 3 apples away you got remaining ${apples}\n`);
+//Declare two variables, firstName and lastName, then create a third variable fullName that combines them. Log the result.
+let firstName = "Muhammad";
+let lastName = "Suleman";
+let fullName = firstName + lastName;
+console.log(`Full name: ${fullName}\n`);
+//Write a TypeScript code that uses a comparison operator to check if the number 5 is greater than 3. Log "Yes" if true, otherwise log "No".
+let number = 5;
+if (number > 3) {
+    console.log("YES\n");
+}
+else {
+    console.log("NO\n");
+}
